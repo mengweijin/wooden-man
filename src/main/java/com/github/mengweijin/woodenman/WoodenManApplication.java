6ced3d5661126d5256c5354c34cc808266bafd94
mengweijin <1002284406@qq.com>
2022-11-12 19:50:15 +0800

    generator

---
+++ src/main/java/com/github/mengweijin/woodenman/WoodenManApplication.java
@@ -0,0 +1,15 @@
+package com.github.mengweijin.woodenman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengweijin
+ */
+@SpringBootApplication
+public class WoodenManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodenManApplication.class, args);
+	}
+}
